9e838c97b550b5ed818b3cb9f7ed3f606600e46a
Austin Vollman <austinvollman@Austins-MacBook-Pro.local>
2021-03-02 13:32:50 -0600

    got db up and running

--- Server/index.js
+++ Server/index.js
@@ -1,4 +1,8 @@
+/* eslint-disable camelcase */
 const path = require('path');
+
+const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 const express = require('express');
 const passport = require('passport');
 const session = require('express-session');
@@ -7,30 +11,27 @@ const cookieParser = require('cookie-parser');
 
 const dotenv = require('dotenv');
 dotenv.config({
-  path: path.resolve(__dirname, '../.env'),
+  path: path.resolve(__dirname, '../.env')
 });
-
-const port = process.env.PORT || 8080;
-
+port = process.env.PORT || 8080;
 const dist = path.resolve(__dirname, '..', 'client', 'dist');
 const app = express();
 
+const db = require('./db/database.js');
+
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(dist));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cookieParser());
-
 // line 34 - 61 all used for google login
-app.use(
-  session({
-    secret: process.env.clientSecret,
-    saveUninitialized: false,
-    resave: true,
-  }),
-);
+app.use(session({
+  secret: process.env.clientSecret,
+  saveUninitialized: false,
+  resave: true
+}));
 
-app.listen(port, () => {
-  console.log(`Server is listening on http://127.0.0.1:${port}`);
+app.listen(port, function () {
+  console.log('Server is listening on http://127.0.0.1:' + port);
 });
