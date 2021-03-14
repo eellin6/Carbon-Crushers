c7c4b8a99677fa2d6817bfc6fd0362323ef12684
Austin Vollman <austinvollman@Austins-MacBook-Pro.local>
2021-03-14 14:24:38 -0500

    graphs render to leaderboard

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -15,7 +15,8 @@ const Footer: React.FC = () => {
         <Link to='/shower'>
           <img src="https://i.ibb.co/k1Y2RT8/crushers-icon-3-timer.png" alt="crushers-icon-3-timer" className="testy"/>
         </Link>
-        <img src="https://i.ibb.co/pwBVWnC/crushers-icon-4-leaderboard.png" alt="crushers-icon-4-leaderboard" className="testy"/>
+        <Link to='/leaderboard'> <img src="https://i.ibb.co/pwBVWnC/crushers-icon-4-leaderboard.png" alt="crushers-icon-4-leaderboard" className="testy"/></Link>
+        {/* <img src="https://i.ibb.co/pwBVWnC/crushers-icon-4-leaderboard.png" alt="crushers-icon-4-leaderboard" className="testy"/> */}
         {/* <img src="https://i.ibb.co/34H6YW5/crushers-icon-5-updates-ALERT.png" alt="crushers-icon-5-updates-ALERT" className="testy"/> */}
         <img src="https://i.ibb.co/C1hNtfb/crushers-icon-5-updates.png" alt="crushers-icon-5-updates" className="testy"/>
         <Link to='/profile'>
--- client/src/components/Leaderboard.tsx
+++ client/src/components/Leaderboard.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { Doughnut } from 'react-chartjs-2';
 const Leaderboard = (): React.ReactElement => {
   const [allStats, setAllStats] = useState(null);
   const temp = [];
@@ -22,6 +23,7 @@ const Leaderboard = (): React.ReactElement => {
             }
 
             temp.sort((a, b) => b.total - a.total);
+            console.info(temp);
             setAllStats(temp);
           })
           .catch((err) => console.warn('Stat Error', err));
@@ -40,14 +42,34 @@ const Leaderboard = (): React.ReactElement => {
 
 
   return (
-    <div>
+    <div className='page-wrap'>
       <h1>Weekly Leaders</h1>
       <hr></hr>
       {!allStats ? null : <div className='LeaderBoard'>
 
         {
           allStats.map((element, index) => <div key={index}>
-            <div >{ element.total} { element.name}</div>
+            <h2 >{ element.total} { element.name}</h2>
+            <Doughnut data={{
+              datasets: [
+                {
+
+                  label: 'First dataset',
+                  data: [element.recycling, element.water, element.meat_dine, element.mileage, element.energy],
+                  fill: true,
+                  backgroundColor: [
+                    '#55BFBF',
+                    '#3EA4E8',
+                    '#FA6685',
+                    '#FC9E4B',
+                    '#FDCB60'
+                  ],
+                  borderColor: ['#FFF'],
+                }
+              ],
+              labels: ['Recycling', 'Water Consumption', 'Meat & Dining Out', 'Mileage', 'Energy']
+            }} />
+
 
 
           </div>)}
