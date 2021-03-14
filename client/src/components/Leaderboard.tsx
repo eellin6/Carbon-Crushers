8c19bc94639a04f6cdc927a0576bbdf66d15852f
Austin Vollman <austinvollman@Austins-MacBook-Pro.local>
2021-03-14 12:23:16 -0500

    Leaderboard working correctly

--- client/src/components/Leaderboard.tsx
+++ client/src/components/Leaderboard.tsx
@@ -2,52 +2,60 @@ import * as React from 'react';
 import { useState, useEffect } from 'react';
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 const Leaderboard = (): React.ReactElement => {
-  const [allStats, setAllStats] = useState([]);
-  const [friends, setFriends] = useState([]);
+  const [allStats, setAllStats] = useState(null);
+  const temp = [];
+
 
   const statistics = (): void => {
     axios.get('/statsData')
       .then(({ data }) => {
         const recent = data[data.length - 1];
-        allStats.push(recent);
+
+        temp.push(recent);
+        return recent;
+      }).then((recent) => {
+        axios.get('/friendsData')
+          .then(( {data} ) => {
+
+            for (let i = 0; i < data.length; i++) {
+              temp.push(data[i]);
+            }
+
+            temp.sort((a, b) => b.total - a.total);
+            setAllStats(temp);
+          })
+          .catch((err) => console.warn('Stat Error', err));
+
+
+
       })
-      .catch((err) => console.warn('Stat Error', err));
-  };
-  const friendsStats = (): void => {
-    axios.get('/friendsData')
-      .then(( {data} ) => {
-        for (let i = 0; i < data.length; i++) {
-          allStats.push(data[i]);
-        }
-      })
-      // .then((friends) => {
-      //   friends.map(friend => axios.get('/friendsData', {friend})
-      //   axios.get('/friendsData', {friends})
-      // })
       .catch((err) => console.warn('Stat Error', err));
   };
 
+
   useEffect(() => {
     statistics();
-    friendsStats();
   }, []);
-  console.info('SORTED', allStats);
+
+
 
   return (
     <div>
       <h1>Weekly Leaders</h1>
       <hr></hr>
-      {!allStats ? [] : (<div>
-        {allStats.map((element) => {
-          console.info('ELEMENT NAME', element.name);
-          return (
-            <div>
-              <div>{element.name}</div>
-              <div>{element.total}</div>
-            </div>);
-        })}
+      {!allStats ? null : <div className='LeaderBoard'>
 
-      </div>)}
+        {
+          allStats.map((element, index) => <div key={index}>
+            <div >{ element.total} { element.name}</div>
+
+
+          </div>)}
+
+      </div> }
+
+
+
 
 
 
