37d012b5eb79f5c3ba7bcc46e673c836eecf3849
Caity Opelka <caityopelka@gmail.com>
2021-03-20 20:56:45 -0500

    UPDATE / stats button increment decrement styling

--- client/src/components/Stat Entries/Meat_Dine.tsx
+++ client/src/components/Stat Entries/Meat_Dine.tsx
@@ -22,18 +22,26 @@ const Meat_Dine = ({meat, dine, func}): React.ReactElement => {
     <div>
       <h2>Log your meat consumption for the week</h2>
       <h3>{meatCount}</h3>
-      <ButtonGroup size="small" aria-label="small outlined button group">
-        {displayMeatCounter && <Button onClick={handleMeatDecrement}>-</Button>}
-        <Button disabled></Button>
-        <Button onClick={handleMeatIncrement}>+</Button>
-      </ButtonGroup>
+      <div className='stats-btns-meat-container'>
+        <div className='stats-btns'>
+          <ButtonGroup size="small" aria-label="small outlined button group">
+            {displayMeatCounter && <Button onClick={handleMeatDecrement}>-</Button>}
+            <Button disabled></Button>
+            <Button onClick={handleMeatIncrement}>+</Button>
+          </ButtonGroup>
+        </div>
+      </div>
       <h2>Log your dining out for the week</h2>
       <h3>{dineCount}</h3>
-      <ButtonGroup size="small" aria-label="small outlined button group">
-        {displayDineCounter && <Button onClick={handleDineDecrement}>-</Button>}
-        <Button disabled></Button>
-        <Button onClick={handleDineIncrement}>+</Button>
-      </ButtonGroup>
+      <div className='stats-btns-meat-container'>
+        <div className='stats-btns'>
+          <ButtonGroup size="small" aria-label="small outlined button group">
+            {displayDineCounter && <Button onClick={handleDineDecrement}>-</Button>}
+            <Button disabled></Button>
+            <Button onClick={handleDineIncrement}>+</Button>
+          </ButtonGroup>
+        </div>
+      </div>
       <h4>14.8 pounds of C02 is produced per pound of meat. The average American eats 3.7 pounds of meat per week, can you beat this? </h4>
       <h4>1697 lbs C02 per household per year is generated from dining out. </h4>
 
